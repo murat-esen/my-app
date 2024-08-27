11e3d74b1c2f77ae4d1f59783fff2b3e4b037688
Murat <muratesen2345@gmail.com>
2024-08-27 11:35:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+ import LoginForm from './Components/LoginForm/LoginForm';
+// import RegisterForm from './Components/RegisterForm/RegisterForm';
+
+function App() {
+  return (
+  
+      <div>
+        <LoginForm />
+      </div>
+        
+     
+  );
+}
+
+export default App;
